b7d4682e831711b9098f8b7112f563d0b326f886
kuasha227 <kuasha227@gmail.com>
2023-06-19 15:35:27 +0600

    commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -3,43 +3,40 @@
 // Website related settings
 const settings = {
   isSplash: true, // Change this to true if you want to use the splash screen.
-  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  useCustomCursor: false, // Change this to false if you want the good'ol cursor
 };
 
 //Home Page
 const greeting = {
   title: "Hi",
-  title2: " I'm Sumit Sharma",
-  logo_name: "Sumit Sharma",
-  // nickname: "harry / picleric",
-  full_name: "Sumit Sharma",
-  subTitle1: "Full Stack Developer.",
+  title2: " I'm Ashiqur Rahman",
+  logo_name: "Ashiqur Rahman",
+
+  full_name: "Ashiqur Rahman",
+  subTitle1: "Frontend Developer.",
   subTitle2: "Life Long Learner.",
-  subTitle: "Full Stack Developer, Life Long Learner.",
+  subTitle: "Professional Web Developer, Life Long Learner.",
   resumeLink:
     "https://drive.google.com/file/d/1lFqkZnNUg5N2muUuv6qIF6so6xloY6-4/view?usp=sharing",
-  mail: "mailto:sumitsharma9968@gmail.com",
+  mail: "mailto:ashiqurrahman356@gmail.com",
 };
 
 const socialMediaLinks = {
-  /* Your Social Media Link */
-  github: "https://github.com/sumit-sharma-02",
-  linkedin: "https://www.linkedin.com/in/sumitsharma002",
-  gmail: "sumitsharma9968@gmail.com",
-  twitter: "https://twitter.com/sumitsharma002",
+  github: "https://github.com/kuasha227",
+  linkedin: "https://www.linkedin.com/in/ashiqur-rahman-ashik/",
+  gmail: "ashiqurrahman356@gmail.com",
+  twitter: "https://twitter.com/ashiqur356",
 };
 
 const skills = {
   data: [
     {
       _id: "354c54b7-ffd8-40f1-972f-1d006733df25",
-      title: "Full Stack Development",
+      title: "Frontend Development",
       fileName: "FullStackImg",
       skills: [
-        "⚡ Develop highly interactive user interfaces and backend for web applications",
-        "⚡ Building responsive website front end using React and Angular",
-        "⚡ Creating application backend in Node, Express, MongoDB and Rest API",
-        "⚡ Integration of third party services such as Firebase, Vercel etc.",
+        "⚡ Develop highly interactive user interfaces",
+        "⚡ Building responsive website front end using React",
       ],
       softwareSkills: [
         {
@@ -65,13 +62,6 @@ const skills = {
           },
         },
         {
-          skillName: "jQuery",
-          fontAwesomeClassname: "simple-icons:jquery",
-          style: {
-            color: "#0769ad",
-          },
-        },
-        {
           skillName: "ReactJS",
           fontAwesomeClassname: "simple-icons:react",
           style: {
@@ -86,27 +76,6 @@ const skills = {
           },
         },
         {
-          skillName: "Angular",
-          fontAwesomeClassname: "simple-icons:angular",
-          style: {
-            color: "#dd0031",
-          },
-        },
-        {
-          skillName: "NodeJS",
-          fontAwesomeClassname: "simple-icons:node-dot-js",
-          style: {
-            color: "#339933",
-          },
-        },
-        {
-          skillName: "Express",
-          fontAwesomeClassname: "simple-icons:express",
-          style: {
-            color: "gray",
-          },
-        },
-        {
           skillName: "Material UI",
           fontAwesomeClassname: "simple-icons:uikit",
           style: {
@@ -116,15 +85,6 @@ const skills = {
         {
           skillName: "Tailwind CSS",
           fontAwesomeClassname: "logos:tailwindcss-icon",
-          style: {
-            color: "#635BFF",
-            width: "1em",
-            height: "1em",
-          },
-        },
-        {
-          skillName: "Framer Motion",
-          fontAwesomeClassname: "logos:framer",
           style: {
             color: "#635BFF",
             width: "1em",
@@ -148,49 +108,10 @@ const skills = {
           },
         },
         {
-          skillName: "Postman",
-          fontAwesomeClassname: "simple-icons:postman",
-          style: {
-            color: "#FF6C37",
-          },
-        },
-        {
           skillName: "NPM",
           fontAwesomeClassname: "simple-icons:npm",
           style: {
             color: "#CB3837",
-          },
-        },
-        {
-          skillName: "MongoDB",
-          fontAwesomeClassname: "simple-icons:mongodb",
-          style: {
-            color: "#439743",
-          },
-        },
-        {
-          skillName: "Cloudinary",
-          fontAwesomeClassname: "logos:cloudinary-icon",
-          style: {
-            color: "#635BFF",
-            width: "1em",
-            height: "1em",
-          },
-        },
-        {
-          skillName: "Stripe",
-          fontAwesomeClassname: "logos:stripe",
-          style: {
-            color: "#635BFF",
-            width: "1em",
-            height: "1em",
-          },
-        },
-        {
-          skillName: "Java",
-          fontAwesomeClassname: "simple-icons:java",
-          style: {
-            color: "#f89820",
           },
         },
         {
@@ -201,31 +122,10 @@ const skills = {
           },
         },
         {
-          skillName: "PostgreSQL",
-          fontAwesomeClassname: "simple-icons:postgresql",
-          style: {
-            color: "#4479A1",
-          },
-        },
-        {
           skillName: "Git",
           fontAwesomeClassname: "simple-icons:git",
           style: {
             color: "#E94E32",
-          },
-        },
-        {
-          skillName: "C++",
-          fontAwesomeClassname: "simple-icons:cplusplus",
-          style: {
-            color: "#E94E32",
-          },
-        },
-        {
-          skillName: "Python",
-          fontAwesomeClassname: "simple-icons:python",
-          style: {
-            color: "#3776AB",
           },
         },
       ],
@@ -329,28 +229,16 @@ const skills = {
 const degrees = {
   degrees: [
     {
-      title: "Amrita Vishwa Vidyapeetham, Coimbatore",
-      subtitle: "Masters in Computer Applications (MCA)",
-      logo_path: "amrita_logo.png",
-      alt_name: "Amrita University",
-      duration: "2021 - 2023",
+      title: "Daffodil International University, Dhaka",
+      subtitle: "Bachelor in Computer Science and Engineering",
+      logo_path: "daffodil-logo.png",
+      alt_name: "Daffodil International University",
+      duration: "2016 - 2020",
       descriptions: [
-        "⚡ I have taken varity of courses related to Artificial Intelligence, Machine Learning, Computer Vision etc.",
-        "⚡ Apart from this, I have done courses on Cloud Computing and Full Stack Development.",
+        "⚡ I have taken variety of courses related to Computer Science, Machine Learning, Computer Vision etc.",
+        "⚡ Apart from this, I have done courses on Algorithms,Data Structure and Frontend Development.",
       ],
-      website_link: "https://www.amrita.edu/",
-    },
-    {
-      title: "Guru Gobind Singh Indraprastha University, New Delhi",
-      subtitle: "Bachelors in Computer Applications (BCA)",
-      logo_path: "ggsipu_logo.png",
-      alt_name: "GGSIPU",
-      duration: "2018 - 2021",
-      descriptions: [
-        "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
-        // "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development."
-      ],
-      website_link: "http://www.ipu.ac.in/",
+      website_link: "https://daffodilvarsity.edu.bd/",
     },
   ],
 };
@@ -361,8 +249,7 @@ const certifications = {
       title: "Frontend Developer Capstone",
       subtitle: "",
       logo_path: "meta_logo.png",
-      certificate_link:
-        "https://coursera.org/share/f80912b58e3afcfa49e6116210a2c9da",
+      certificate_link: "",
       alt_name: "Meta",
       color_code: "#096ce1",
     },
@@ -370,8 +257,7 @@ const certifications = {
       title: "Advanced React",
       subtitle: "",
       logo_path: "meta_logo.png",
-      certificate_link:
-        "https://coursera.org/share/877acd7eaf9dd532a2cb5b103576967e",
+      certificate_link: "",
       alt_name: "Meta",
       color_code: "#096ce1",
     },
@@ -379,8 +265,7 @@ const certifications = {
       title: "Java Programming: Build a Recommendation System",
       subtitle: "",
       logo_path: "duke-logo.jpg",
-      certificate_link:
-        "https://coursera.org/share/6af001cafc4b683bb38abdb9e81d0d79",
+      certificate_link: "",
       alt_name: "Duke University",
       color_code: "#001a57",
     },
@@ -388,8 +273,7 @@ const certifications = {
       title: "React Basics",
       subtitle: "",
       logo_path: "meta_logo.png",
-      certificate_link:
-        "https://coursera.org/share/29c75a387fcaf2f1e2403f73473092b2",
+      certificate_link: "",
       alt_name: "Meta",
       color_code: "#096ce1",
     },
@@ -397,8 +281,7 @@ const certifications = {
       title: "Programming with JavaScript",
       subtitle: "",
       logo_path: "meta_logo.png",
-      certificate_link:
-        "https://coursera.org/share/cb44f1ee2db3f50b83a56cbae15fcc74",
+      certificate_link: "",
       alt_name: "Meta",
       color_code: "#096ce1",
     },
@@ -406,100 +289,25 @@ const certifications = {
       title: "HTML and CSS in depth",
       subtitle: "",
       logo_path: "meta_logo.png",
-      certificate_link:
-        "https://coursera.org/share/667de02731a480b76b5300c6612d174d",
+      certificate_link: "",
       alt_name: "Meta",
       color_code: "#096ce1",
-    },
-    {
-      title: "Java Programming: Principles of Software Design",
-      subtitle: "",
-      logo_path: "duke-logo.jpg",
-      certificate_link:
-        "https://coursera.org/share/f4e9af47fbb24381e04031f3cc8cfd9f",
-      alt_name: "Duke University",
-      color_code: "#001a57",
-    },
-    {
-      title: "Java Programming: Solving Problems with Software",
-      subtitle: "",
-      logo_path: "duke-logo.jpg",
-      certificate_link:
-        "https://coursera.org/share/36411fcea7ea4fbb5c613ef4911f4668",
-      alt_name: "Duke University",
-      color_code: "#001a57",
     },
     {
       title: "Programming Foundations with JavaScript, HTML and CSS",
       subtitle: "",
       logo_path: "duke-logo.jpg",
-      certificate_link:
-        "https://coursera.org/share/0269c76c33997304d1842d4eef16815b",
+      certificate_link: "",
       alt_name: "Duke University",
       color_code: "#001a57",
     },
     {
-      title: "Android Application Development - II",
-      subtitle: "- Dr. Adam Porter",
-      logo_path: "maryland_logo.svg",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/GN48B7W9TU59",
-      alt_name: "Stanford University",
-      color_code: "#e11a2b",
-    },
-    {
-      title: "Android Application Development - I",
-      subtitle: "- Dr. Adam Porter",
-      logo_path: "maryland_logo.svg",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/2H59Z22DDSU8",
-      alt_name: "Stanford University",
-      color_code: "#e11a2b",
-    },
-    {
-      title: "Fullstack Development",
+      title: "Frontend Development",
       subtitle: "- Christian Hur",
       logo_path: "coursera_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/97WF8EZ4Y2YD",
+      certificate_link: "",
       alt_name: "Coursera",
       color_code: "#0056d2",
-    },
-    {
-      title: "ML and AI in GCP",
-      subtitle: "- GCP Training",
-      logo_path: "gcp_logo.png",
-      certificate_link:
-        "https://www.cloudskillsboost.google/public_profiles/345a17d9-689f-4154-a3d3-c0396c98b3bc/badges/2862832",
-      alt_name: "GCP",
-      color_code: "#2fa9508a",
-    },
-    {
-      title: "Foundational Infrastructure in GCP",
-      subtitle: "- GCP Training",
-      logo_path: "gcp_logo.png",
-      certificate_link:
-        "https://www.cloudskillsboost.google/public_profiles/345a17d9-689f-4154-a3d3-c0396c98b3bc/badges/2653409",
-      alt_name: "GCP",
-      color_code: "#4087f08a",
-    },
-    {
-      title: "Networking & Security in GCP",
-      subtitle: "- GCP Training",
-      logo_path: "gcp_logo.png",
-      certificate_link:
-        "https://www.cloudskillsboost.google/public_profiles/345a17d9-689f-4154-a3d3-c0396c98b3bc/badges/2510430",
-      alt_name: "GCP",
-      color_code: "#eb40308a",
-    },
-    {
-      title: "Cloud Computing Fundamentals in GCP",
-      subtitle: "- GCP Training",
-      logo_path: "gcp_logo.png",
-      certificate_link:
-        "https://www.cloudskillsboost.google/public_profiles/345a17d9-689f-4154-a3d3-c0396c98b3bc/badges/2469792",
-      alt_name: "GCP",
-      color_code: "#fabd008a",
     },
   ],
 };
@@ -509,45 +317,45 @@ const experience = {
   title: "Experience",
   subtitle: "Work and Apprenticeship",
   description:
-    "I've over 2 years of experience as a full stack developer. I've worked on building products for multiple stakeholders in Infosys as well as my personal projects.",
+    "I've over 1 year of experience as a frontend developer. I've worked on building products for multiple stakeholders in Infosys as well as my personal projects.",
   header_image_path: "experience.svg",
   sections: {
     title: "Work Experience",
     experiences: [
       {
-        title: "System Associate",
-        company: "Infosys",
-        company_url: "https://www.infosys.com/",
-        logo_path: "infosys-logo.png",
-        alt_name: "infosys-logo",
-        duration: "(Feb, 2023 - May, 2023)",
+        title: "Website Developer",
+        company: "Happy Touch",
+        company_url: "https://www.facebook.com/happytouch.com.bd",
+        logo_path: "happpytouch.png",
+        alt_name: "Happy Touch-logo",
+        duration: "(Jan, 2022 - May, 2022)",
         location: "Remote",
         description:
-          "<li>Launched Encharta 2.0 in a team of four with add on features such as multiple content pages in a single project, audio, custom gradients, trigger and hover based animations etc. in canvas using ReactJS v18.</li><br><li>Created a PWA named Encharta for all the educators in Infosys for creating their own analogies with add on animations in their content using HTML, CSS, JavaScript and an open source library Fabric.js.</li>",
+          "<li>Launched Happy Touch E-Commerce in a team of four with add on features such as multiple content pages in a single project, audio, custom gradients, trigger and hover based animations etc. in canvas using ReactJS v18.</li><br><li>Maintained the whole website and focus on user friendly customization</li>",
         color: "#0071C5",
       },
       {
-        title: "Operations Executive",
-        company: "Infosys",
-        company_url: "https://www.infosys.com/",
-        logo_path: "infosys-logo.png",
-        alt_name: "infosys-logo",
-        duration: "(Nov, 2021 - Jan, 2023)",
+        title: "Frontend Developer",
+        company: "Digibook Limited",
+        company_url: "https://www.facebook.com/digibooklimited.bd",
+        logo_path: "digibook.png",
+        alt_name: "Digibook-logo",
+        duration: "(Jun, 2022 - Dec, 2022)",
         location: "Remote",
         description:
-          "<li>Implemented few UI tweaks and added new categories with add on pop-ups (video, images etc.) in an web app named “Awards For Excellence” which is used for nominating employees within Infosys for various award categories using Angular v10.</li><br><li>Implemented REST API using Node and Express to fetch the data of the classrooms, learners, educators and courses. Used socket programming for multi-user experience and other features like chat, screen, audio and video share for web based AR/VR learning platform.</li><br><li>Developed 2 new tools with an intuitive design in Lex (Learning platform for the employees of Infosys) for interactive content creation using Angular v12 which is getting used by 100+ educators for creating new interactive courses, consumed by 1000+ learners.</li>",
+          "<li>Implemented implemented a super responsive frontend e-commerce with seo and bug fixing & maintaining continuously</br> Still now continuously maintaining their websites. I have developed 4 websites under Digibook Limited.</br>I have also worked there as a Business Consultant. I have developed 2 account software under them.</li>",
         color: "#0071C5",
       },
       {
-        title: "Java Full Stack Developer Trainee",
-        company: "Infosys",
-        company_url: "https://www.infosys.com/",
-        logo_path: "infosys-logo.png",
-        alt_name: "infosys-logo",
-        duration: "(Aug, 2021 - Nov, 2021)",
+        title: "Website Developer",
+        company: "Coachcouncil",
+        company_url: "https://www.coachcouncil.com.bd/",
+        logo_path: "coachcouncil.png",
+        alt_name: "coachcouncil-logo",
+        duration: "(Nov, 2022 - Feb, 2023)",
         location: "Remote",
         description:
-          "Worked on multiple Capstone projects using Spring Boot and Angular.",
+          "This is a sister concern of Digibook Limited. I have developed their lms system on their website and maintained them regularly </br> First I have developed their frontend. Then I have gone for the lms system. They are enrolling students regularly and genenrating profits continuously. I also maintained their website speed optomization",
         color: "#4285F4",
       },
     ],
@@ -581,32 +389,12 @@ const projects = {
           iconifyClass: "logos:redux",
         },
         {
-          name: "Express",
-          iconifyClass: "simple-icons:express",
-        },
-        {
-          name: "Nodejs",
-          iconifyClass: "logos:nodejs",
-        },
-        {
-          name: "MongoDB",
-          iconifyClass: "vscode-icons:file-type-mongo",
-        },
-        {
           name: "Stripe",
           iconifyClass: "logos:stripe",
         },
         {
           name: "Tailwind",
           iconifyClass: "vscode-icons:file-type-tailwind",
-        },
-        {
-          name: "Cloudinary",
-          iconifyClass: "logos:cloudinary-icon",
-        },
-        {
-          name: "Framer Motion",
-          iconifyClass: "logos:framer",
         },
       ],
     },
@@ -633,10 +421,6 @@ const projects = {
         {
           name: "Tailwind",
           iconifyClass: "vscode-icons:file-type-tailwind",
-        },
-        {
-          name: "Framer Motion",
-          iconifyClass: "logos:framer",
         },
       ],
     },
